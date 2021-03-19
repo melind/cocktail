1644996549b1d07c8c91b0ae6141d5b8394fb7d5
Mélinda M <melinda93@hotmail.fr>
2021-03-19 15:47:29 +0100

    fixedadmin

--- backend/middlewares/adminMiddleware.js
+++ backend/middlewares/adminMiddleware.js
@@ -1,3 +1,4 @@
+
 exports.__esModule = true;
 
 var jsonwebtoken = require("jsonwebtoken");
@@ -6,31 +7,31 @@ function default_1 (request, response, next) {
   // check cookie presence and good jwt
 
   // no need to check for this pages so we get their url (http://....)
-  if ( ['/admin-938-kml'].includes(request.url) ) {
+  if ( ['/account','/update-mail','/update-password', '/update-user-name','/logout','/admin-938-kml'].includes(request.url) ) {
     const token = request.cookies.jwt;
      // @ts-ignore
     const csrf= request.session.csrf;
  
     try {
        // @ts-ignore
-      const decodedToken=  jsonwebtoken.verify(token,process.env.JWT_PRIVATE_KEY);
+      const decodedToken= jsonwebtoken.verify(token,process.env.JWT_PRIVATE_KEY);
          // @ts-ignore
-        if ((decodedToken.mail === process.env.MAIL ) && csrf) {
+        if (decodedToken && csrf) {
            // @ts-ignore
         
 
         next();
       } else {
-        response.status(403).end();
+        response.status(401).end();
       }
     } catch (error) {
       
       
-      response.status(403).end();
+      response.status(401).end();
     }
     } else {
 
-    response.status(403).end();
+    response.status(401).end();
   }
 }
 
