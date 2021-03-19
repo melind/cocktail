f3bf1bc35ce7e3ace22bbfa9d4c1b7d7323c5b00
Mélinda M <melinda93@hotmail.fr>
2021-03-19 16:27:17 +0100

    fixedadmin

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,4 +1,4 @@
-
+/*
 exports.__esModule = true;
 
 var jsonwebtoken = require("jsonwebtoken");
@@ -35,4 +35,32 @@ function default_1 (request, response, next) {
   }
 }
 
+exports["default"] = default_1;
+*/
+"use strict";
+exports.__esModule = true;
+var jsonwebtoken = require("jsonwebtoken");
+function default_1(request, response, next) {
+    // check cookie presence and good jwt
+    // no need to check for this pages so we get their url (http://....)
+    if (['/', '/login', '/signup','home'].includes(request.url)) {
+        next();
+    }
+    else {
+        var token = request.cookies.jwt;
+        var csrf = request.session.csrf;
+        try {
+            var decodedToken = jsonwebtoken.verify(token, process.env.JWT_PRIVATE_KEY);
+            if (decodedToken.admin  && csrf) {
+                next();
+            }
+            else {
+                response.status(403).end();
+            }
+        }
+        catch (error) {
+            response.status(403).end();
+        }
+    }
+}
 exports["default"] = default_1;
