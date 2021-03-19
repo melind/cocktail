8fdf064baf7ea12767112bbe10401b48900b9624
Mélinda M <melinda93@hotmail.fr>
2021-03-19 16:51:16 +0100

    fixedadmin

--- backend/middlewares/adminMiddleware.js
+++ backend/middlewares/adminMiddleware.js
@@ -1,20 +1,20 @@
-/*
+"use strict";
 exports.__esModule = true;
 
 var jsonwebtoken = require("jsonwebtoken");
 
 function default_1 (request, response, next) {
   // check cookie presence and good jwt
-
+console.log(request.cookies.jwt);
   // no need to check for this pages so we get their url (http://....)
-  if ( ['/account','/update-mail','/update-password', '/update-user-name','/logout','/admin-938-kml'].includes(request.url) ) {
-    const token = request.cookies.jwt;
+  if ( ['/admin-938-kml'].includes(request.url) ) {
+    var token = request.cookies.jwt;
      // @ts-ignore
-    const csrf= request.session.csrf;
+    var csrf = request.session.csrf;
  
     try {
        // @ts-ignore
-      const decodedToken=  jsonwebtoken.verify(token,process.env.JWT_PRIVATE_KEY);
+      var decodedToken=  jsonwebtoken.verify(token,process.env.JWT_PRIVATE_KEY);
          // @ts-ignore
         if ((decodedToken.mail === process.env.MAIL ) && csrf) {
            // @ts-ignore
@@ -35,34 +35,4 @@ function default_1 (request, response, next) {
   }
 }
 
-exports["default"] = default_1;
-*/
-
-"use strict";
-exports.__esModule = true;
-var jsonwebtoken = require("jsonwebtoken");
-function default_1(request, response, next) {
-    // check cookie presence and good jwt
-    // no need to check for this pages so we get their url (http://....)
-    if (['/', '/login', '/signup','home'].includes(request.url)) {
-        next();
-    }
-    else {
-      
-        var token = request.cookies.jwt;
-        var csrf = request.session.csrf;
-        try {
-            var decodedToken = jsonwebtoken.verify(token, process.env.JWT_PRIVATE_KEY);
-            if ((decodedToken.mail === process.env.MAIL) && csrf) {
-                next();
-            }
-            else {
-                response.status(403).end();
-            }
-        }
-        catch (error) {
-            response.status(403).end();
-        }
-    }
-}
 exports["default"] = default_1;
--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,20 +1,20 @@
-/*
+"use strict";
 exports.__esModule = true;
 
 var jsonwebtoken = require("jsonwebtoken");
 
 function default_1 (request, response, next) {
   // check cookie presence and good jwt
-
+  console.log(request.cookies.jwt);
   // no need to check for this pages so we get their url (http://....)
-  if ( ['/account','/update-mail','/update-password', '/update-user-name','/logout','/admin-938-kml'].includes(request.url) ) {
-    const token = request.cookies.jwt;
+  if ( ['/account','/update-mail','/update-password', '/update-user-name','/logout'].includes(request.url) ) {
+    var token = request.cookies.jwt;
      // @ts-ignore
-    const csrf= request.session.csrf;
+    var csrf= request.session.csrf;
  
     try {
        // @ts-ignore
-      const decodedToken= jsonwebtoken.verify(token,process.env.JWT_PRIVATE_KEY);
+      var decodedToken= jsonwebtoken.verify(token,process.env.JWT_PRIVATE_KEY);
          // @ts-ignore
         if (decodedToken && csrf) {
            // @ts-ignore
@@ -35,32 +35,4 @@ function default_1 (request, response, next) {
   }
 }
 
-exports["default"] = default_1;
-*/
-"use strict";
-exports.__esModule = true;
-var jsonwebtoken = require("jsonwebtoken");
-function default_1(request, response, next) {
-    // check cookie presence and good jwt
-    // no need to check for this pages so we get their url (http://....)
-    if (['/', '/login', '/signup','home'].includes(request.url)) {
-        next();
-    }
-    else {
-        var token = request.cookies.jwt;
-        var csrf = request.session.csrf;
-        try {
-            var decodedToken = jsonwebtoken.verify(token, process.env.JWT_PRIVATE_KEY);
-            if (decodedToken.admin  && csrf) {
-                next();
-            }
-            else {
-                response.status(403).end();
-            }
-        }
-        catch (error) {
-            response.status(403).end();
-        }
-    }
-}
 exports["default"] = default_1;
