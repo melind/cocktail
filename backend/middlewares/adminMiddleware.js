99a5d846f10d877c0556f54f0b2ca37a227a7cf2
Mélinda M <melinda93@hotmail.fr>
2021-03-19 14:03:21 +0100

    fixedadmin

--- backend/middlewares/adminMiddleware.js
+++ backend/middlewares/adminMiddleware.js
@@ -1,4 +1,4 @@
-exports.__esModule = true;
+/*exports.__esModule = true;
 
 var jsonwebtoken = require("jsonwebtoken");
 
@@ -21,19 +21,57 @@ function default_1 (request, response, next) {
 
         next();
       } else {
-        response.status(403).end();
+        response.status(401).end();
       }
     } catch (error) {
       
       
-      response.status(403).end();
+      response.status(401).end();
     }
     } else {
 
-    response.status(403).end();
+    response.status(401).end();
   }
 }
 
 exports["default"] = default_1;
+*/
 
 
+exports.__esModule = true;
+
+var jsonwebtoken = require("jsonwebtoken");
+
+function default_1 (request, response, next) {
+  // check cookie presence and good jwt
+
+  // no need to check for this pages so we get their url (http://....)
+  if ( ['/account','/update-mail','/update-password', '/update-user-name','/logout','/admin-938-kml'].includes(request.url) ) {
+    const token = request.cookies.jwt;
+     // @ts-ignore
+    const csrf= request.session.csrf;
+ 
+    try {
+       // @ts-ignore
+      const decodedToken= jsonwebtoken.verify(token,process.env.JWT_PRIVATE_KEY);
+         // @ts-ignore
+        if (decodedToken && csrf) {
+           // @ts-ignore
+        
+
+        next();
+      } else {
+        response.status(401).end();
+      }
+    } catch (error) {
+      
+      
+      response.status(401).end();
+    }
+    } else {
+
+    response.status(401).end();
+  }
+}
+
+exports["default"] = default_1;
